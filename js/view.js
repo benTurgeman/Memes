306e0b8e1800f25631da22cfe749d8e9ce32ea92
unknown <ben123tu@gmail.com>
2017-11-17 17:11:41 +0200

    stuff

--- js/view.js
+++ js/view.js
@@ -1,3 +1,24 @@
 'use strict'
 
+// function toggleNav() {
+//     var elNav = document.querySelector('.nav-buttons')
+//     if (!elNav.classList.contains('fadeInDown')) {
+//         elNav.classList.add('fadeInDown')
+//     }else{
+//         elNav.classList.add('fadeOutDown')
+//     }
+// }
 
+
+function toggleNav() {
+    var elNav = document.querySelector('.nav-buttons')
+    if (elNav.classList.contains('fadeInDown') || elNav.classList.contains('fadeOutDown')) {
+        elNav.classList.toggle('fadeInDown' , 'fadeOutUp');
+        // elNav.classList.toggle('fadeOutUp');
+        
+
+    }else{
+        elNav.classList.add('fadeInDown')
+    }
+
+}
