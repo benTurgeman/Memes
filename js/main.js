e43973aa460bdd43dd85b498f79bade54b17bcb6
unknown <ben123tu@gmail.com>
2017-11-20 17:45:33 +0200

    almost there

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var gCanvas;
+
 var gMeme = {
     selectedImgId: '',
     selectedElImg: '',
@@ -9,26 +11,28 @@ var gMeme = {
             line: '',
             size: 20,
             align: 'left',
-            color: 'black'
+            color: 'black',
+            height: 30
         },
         {
             line: '',
             size: 20,
             align: 'left',
-            color: 'black'
-        }
+            color: 'black',
+            height: 150
+        },
     ]
 }
 
 function updateMemeImage(elImg) {
-    var img = elImg
+    var img = elImg;
     gMeme.selectedImgId = elImg.id;
     renderCanvas()
 }
 
-function updateMemeText(idx) {
-    var inputBox = document.querySelector(`#text${idx}`)
-    gMeme.txts[idx].line = inputBox.value
+function updateMemeText(idx, elInput) {
+    var inputBox = elInput.value;
+    gMeme.txts[idx].line = inputBox;
     renderCanvas()
 }
 
@@ -54,23 +58,56 @@ function updateMemeTextColor(idx, elInput) {
     renderCanvas()
 }
 
+function changeTextHeight(idx ,value) {
+    switch (value) {
+        case 'plus':
+            gMeme.txts[idx].height -= 5
+            break;
+        case 'minus':
+            gMeme.txts[idx].height += 5
+            break;
+        default:
+            break;
+    }
+    renderCanvas()
+}
 
 
 
 function renderCanvas() {
     var ctx = gMeme.ctx;
     var img = gMeme.selectedElImg;
-    var txtAlign = alignText(0 , img)
-    ctx.drawImage(img, 0, 0, 388, 150);
+    var txtAlign = alignText(0, gCanvas)
+    ctx.drawImage(img, 0, 0, 340, 260);
+    ctx.restore()
     ctx.font = '400 ' + gMeme.txts[0].size + 'px Arial, sans-serif';
     ctx.fillStyle = gMeme.txts[0].color;
     ctx.textAlign = gMeme.txts[0].align;
-    ctx.fillText(gMeme.txts[0].line, txtAlign , 30)
-    var txtAlign = alignText(1 , img)
+    ctx.fillText(gMeme.txts[0].line, txtAlign, gMeme.txts[0].height)
+    var txtAlign = alignText(1, gCanvas)
     ctx.font = '400 ' + gMeme.txts[1].size + 'px Arial, sans-serif';
     ctx.fillStyle = gMeme.txts[1].color;
     ctx.textAlign = gMeme.txts[1].align;
-    ctx.fillText(gMeme.txts[1].line, txtAlign , 120)
+    ctx.fillText(gMeme.txts[1].line, txtAlign, gMeme.txts[1].height)
+}
+
+
+function renderWorkZone(elImg) {
+    var img = elImg
+    var canvas = document.querySelector('.canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = 340;
+    canvas.height = 260;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+    gMeme.selectedImgId = +elImg.id;
+    gMeme.selectedElImg = elImg;
+    gMeme.ctx = ctx;
+    gCanvas = canvas;
+
+    var elImgs = document.querySelector('.images-container')
+    var elCanvas = document.querySelector('.canvas-modal')
+    elImgs.style.display = 'none';
+    elCanvas.style.display = 'flex';
 }
 
 function alignText(idx, img) {
@@ -79,7 +116,7 @@ function alignText(idx, img) {
             return 0
             break;
         case 'center':
-            return (img.width/2)
+            return (img.width / 2)
             break;
         case 'right':
             return img.width
@@ -87,22 +124,19 @@ function alignText(idx, img) {
     }
 }
 
-function renderWorkZone(elImg) {
-    var img = elImg
-    var canvas = document.querySelector('.canvas');
-    var ctx = canvas.getContext('2d');
-    canvas.width = img.width
-    canvas.height = img.height
-    ctx.drawImage(img, 0, 0, img.width, img.height, )
-    gMeme.selectedImgId = +elImg.id;
-    gMeme.selectedElImg = elImg
-    gMeme.ctx = ctx
+// function addLine() {
+//     gMeme.txts.push({
+//         line: '',
+//         size: 20,
+//         align: 'left',
+//         color: 'black',
+//         height: 30
+//     }, )
+//     gMeme.lineIdx++
+//     document.querySelector('.canvas-text-input').value = ''
+// }
 
-    var elImgs = document.querySelector('.images-container')
-    var elCanvas = document.querySelector('.canvas-modal')
-    elImgs.style.display = 'none';
-    elCanvas.style.display = 'flex';
+function downloadMeme(elBtn){
+    elBtn.href = document.querySelector('.canvas').toDataURL();
+    elBtn.download = 'YourMeme.png';
 }
-
-
-
--- js/view.js
+++ js/view.js
@@ -1,18 +1,38 @@
 'use strict'
+
+
 var gImgs;
 
 function initPage() {
-    tempImgs()
-    renderImgs()
+    getImgs()
+    renderImgs(gImgs)
 }
 
-function toggleNav() {
-    var elNav = document.querySelector('.nav-wrapper')
-    elNav.classList.toggle('hide')
+function uploadNewImage(elInput) {
+    var tempCanvas = document.querySelector('.temp-canvas')
+    var canvas = document.querySelector('.canvas')
+    var ctx = canvas.getContext('2d');
+    var reader = new FileReader();
+    var file = elInput.files[0];
+    // load to image to get it's width/height
+    var newImg = new Image();
+    newImg.onload = function () {
+        // scale canvas to image
+        // draw image
+        ctx.drawImage(newImg, 0, 0
+            , canvas.width, canvas.height
+        );
+    }
+    // this is to setup loading the image
+    reader.onloadend = function () {
+        newImg.src = reader.result;
+    }
+    // this is to read the file
+    reader.readAsDataURL(file);
+    renderWorkZone(tempCanvas)
 }
 
 function goBackToImgs() {
-
     var elImgs = document.querySelector('.images-container')
     var elCanvas = document.querySelector('.canvas-modal')
     elImgs.style.display = 'flex';
@@ -27,47 +47,60 @@ function goBackToImgs() {
                 line: '',
                 size: 20,
                 align: 'left',
-                color: 'black'
+                color: 'black',
+                height: 30
             },
             {
                 line: '',
                 size: 20,
                 align: 'left',
-                color: 'black'
+                color: 'black',
+                height: 150
             }
         ]
     }
+    var elInput = document.querySelectorAll('#text')
+    elInput.forEach(function (elText) {
+        elText.value = ''
+    })
 }
 
 
-function renderImgs() {
+function renderImgs(imgs) {
     var elImagesContainer = document.querySelector('.images-container')
     var strHtml = ''
-    gImgs.forEach(function (img) {
+    imgs.forEach(function (img) {
         strHtml +=
             `<div class="image-container">
-        <img class="canvas-image" id="${img.idx}" src="images/${img.idx}.jpg" onclick="renderWorkZone(this)"/>
+        <img class="canvas-image" id="pic-${img.idx}" src="images/${img.idx}.jpg" onclick="renderWorkZone(this)"/>
     </div>`
     });
     elImagesContainer.innerHTML = strHtml
 }
 
 
-function tempImgs() {
-    var imgs = []
-    var img1 = {
+function getImgs() {
+    var imgs = [{
         idx: 0,
-        url: 'images/0.jpg',
-        keywords: ['game of Thrones', 'state', 'funny'],
-    }
-    var img2 = {
+        keywords: ['game of Thrones', 'state', 'funny', 'guy', 'man'],
+    },
+    {
         idx: 1,
-        url: 'images/1.jpg',
-        keywords: ['cartoon', 'wonder', 'funny']
-    }
-
-    imgs.push(img1);
-    imgs.push(img2);
+        keywords: ['cartoon', 'wonder', 'funny', 'man']
+    },
+    {
+        idx: 2,
+        keywords: ['man', 'shouting', 'ars',]
+    },
+    {
+        idx: 3,
+        keywords: ['girl', 'ask', 'wtf']
+    },
+    {
+        idx: 4,
+        keywords: ['ars', 'shout', 'man']
+    },
+    ]
 
     gImgs = imgs;
 }
@@ -85,25 +118,34 @@ function imgSearchBar() {
     var searchWord = document.querySelector('#searchBar');
     var searchTxt = searchWord.value;
     var tempImgs = [];
+    if (searchTxt === 'What are you looking for?') {
+        renderImgs(gImgs)
+        return;
+    }
 
     gImgs.forEach(function (img, idx) {
-        //console.log('img',img.keywords);
+        // console.log('img',img.keywords);
         //console.log('idx',idx);
         var keywords = img.keywords;
 
         keywords.forEach(function (keyword) {
             if (keyword === searchTxt) {
-                tempImgs.push(idx);
+                tempImgs.push(img);
+                console.log(tempImgs)
             }
         });
 
     });
+    renderImgs(tempImgs);
+}
 
-    var imagesOutput = document.querySelector('#imagesOutput');
-    imagesOutput.innerHTML = "";
+function toggleNavButtons() {
+    var navBtns = document.querySelector('.nav-buttons')
+    if (navBtns.style.display == '' || navBtns.style.display == "none") {
+        navBtns.style.display = 'flex';
+    } else {
+        navBtns.style.display = 'none';
+    }
 
-    tempImgs.forEach(function (imgIdx) {
-        imagesOutput.innerHTML += imgIdx;
-    });
 }
 
